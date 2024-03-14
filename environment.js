d9415e070a123840a58547eb05bc04eb82004b11
underpressure98 <118422673+underpressure98@users.noreply.github.com>
2024-03-13 19:25:34 -0500

    Add files via upload

---
+++ environment.js
@@ -0,0 +1,95 @@
+var elementorFrontendConfig = {
+    environmentMode: {
+      edit: false,
+      wpPreview: false,
+      isScriptDebug: false,
+    },
+    i18n: {
+
+    },
+    is_rtl: false,
+    breakpoints: { xs: 0, sm: 480, md: 768, lg: 1025, xl: 1440, xxl: 1600 },
+    responsive: {
+      breakpoints: {
+        mobile: {
+          label: "Mobile",
+          value: 767,
+          default_value: 767,
+          direction: "max",
+          is_enabled: true,
+        },
+        mobile_extra: {
+          label: "Mobile Extra",
+          value: 880,
+          default_value: 880,
+          direction: "max",
+          is_enabled: false,
+        },
+        tablet: {
+          label: "Tablet",
+          value: 1024,
+          default_value: 1024,
+          direction: "max",
+          is_enabled: true,
+        },
+        tablet_extra: {
+          label: "Tablet Extra",
+          value: 1200,
+          default_value: 1200,
+          direction: "max",
+          is_enabled: false,
+        },
+        laptop: {
+          label: "Laptop",
+          value: 1366,
+          default_value: 1366,
+          direction: "max",
+          is_enabled: false,
+        },
+        widescreen: {
+          label: "Widescreen",
+          value: 2400,
+          default_value: 2400,
+          direction: "min",
+          is_enabled: false,
+        },
+      },
+    },
+    version: "3.4.4",
+    is_static: false,
+    experimentalFeatures: {
+      e_dom_optimization: true,
+      a11y_improvements: true,
+      e_import_export: true,
+      additional_custom_breakpoints: true,
+      "hello-theme-header-footer": true,
+      "landing-pages": true,
+      "elements-color-picker": true,
+      "admin-top-bar": true,
+      "form-submissions": true,
+    },
+    urls: {
+      assets:
+        "https:\/\/nobtheentertainer.tech\/wp-content\/plugins\/elementor\/assets\/",
+    },
+    settings: { page: [], editorPreferences: [] },
+    kit: {
+      active_breakpoints: ["viewport_mobile", "viewport_tablet"],
+      global_image_lightbox: "yes",
+      lightbox_enable_counter: "yes",
+      lightbox_enable_fullscreen: "yes",
+      lightbox_enable_zoom: "yes",
+      lightbox_enable_share: "yes",
+      lightbox_title_src: "title",
+      lightbox_description_src: "description",
+      hello_header_logo_type: "logo",
+      hello_header_menu_layout: "horizontal",
+      hello_footer_logo_type: "logo",
+    },
+    post: {
+      id: 417,
+      title: "",
+      excerpt: "",
+      featuredImage: false,
+    },
+  };
